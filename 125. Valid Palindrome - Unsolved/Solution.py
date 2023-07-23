6470c22bd458e24ff5da22c91f283b6bf311fdb1
PengHian Ang <v_fphhang@tencent.com>
2023-07-24 03:30:20 +0800

    update

---
+++ 125. Valid Palindrome - Unsolved/Solution.py
@@ -0,0 +1,13 @@
+s = "A man, a plan, a canal: Panama"
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        print(s)
+        
+        
+        
+        
+        
+        
+solution = Solution()
+print(solution.isPalindrome(s))
---
+++ 238. Product of Array Except Self - Unsolved/Solution.py
@@ -0,0 +1,22 @@
+from typing import List
+
+nums = [1,2,3,4]
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        output = []
+        tmp_sum = 0
+        for index, item in enumerate(nums):
+            
+        print(nums)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+solution = Solution()
+print(solution.productExceptSelf(nums))
